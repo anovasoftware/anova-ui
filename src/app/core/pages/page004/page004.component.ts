eeba5fb3c41c13fdbfb5d9b3cd0022609368cb61
anovasoftware <jburke@anovasoftware.com>
2025-07-02 15:39:24 +0000

    login screen 1

---
+++ src/app/core/pages/page004/page004.component.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import {AuthService} from '../../../services/auth.service';
+import {FormsModule} from '@angular/forms';
+
+@Component({
+  selector: 'app-page004',
+  standalone: true,
+  imports: [
+    FormsModule,
+  ],
+  templateUrl: './page004.component.html',
+  styleUrls: ['./page004.component.scss'],
+})
+export class Page004Component {
+  username: string = 'j';
+  password: string = '';  // 102129!!
+  errorMessage: string = '';
+
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.username = localStorage.getItem('last_username') || '';
+  }
+  onSubmit() {
+    localStorage.setItem('last_username', this.username);
+
+    this.authService.login(this.username, this.password)
+      .subscribe({
+        next: () => {
+          console.log('Login successful!');
+          // Optionally route to another page here
+        },
+        error: (err) => {
+          console.error('Login error', err);
+          this.errorMessage = 'Invalid username or password';
+        }
+      });
+  }
+
+
+}
---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private apiUrl = 'http://localhost:8000/api/token/';
+
+  constructor(private http: HttpClient) {}
+
+  login(username: string, password: string): Observable<any> {
+    return this.http.post<any>(this.apiUrl, { username, password })
+      .pipe(
+        tap(response => {
+          this.storeTokens(response.access, response.refresh);
+        })
+      );
+  }
+
+  storeTokens(access: string, refresh: string) {
+    localStorage.setItem('access_token', access);
+    localStorage.setItem('refresh_token', refresh);
+  }
+
+  getAccessToken(): string | null {
+    return localStorage.getItem('access_token');
+  }
+
+  getRefreshToken(): string | null {
+    return localStorage.getItem('refresh_token');
+  }
+}
