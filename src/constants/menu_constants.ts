4c2d309911da3397fafaf24ccd3c3a8074e2cbcb
anovasoftware <jburke@anovasoftware.com>
2026-04-21 06:57:18 -0400

    constants.1

--- src/constants/menu_constants.ts
+++ src/constants/menu_constants.ts
@@ -1,4 +1,4 @@
 export class MenuConstants {
 // AUTO GENERATED
-  static readonly NOT_APPLICABLE = '000';  static readonly HOME = '001';  static readonly LOGIN = '004';  static readonly MODULES = '003';  static readonly ADMINISTRATION_ANOVA = '005';  static readonly CATEGORY_ROOM = '00C';  static readonly DATABASE_HOTEL = '009';  static readonly EVENT = '00F';  static readonly GANGWAY = '008';  static readonly GUEST = '006';  static readonly PROPERTY_SETUP = '00B';  static readonly ROOM = '00D';  static readonly USER_ANOVA = '007';}
+  static readonly NOT_APPLICABLE = '000';  static readonly HOME = '001';  static readonly LOGIN = '004';  static readonly PRODUCTS = '00G';  static readonly ADMINISTRATION_ANOVA = '005';  static readonly CATEGORY_ROOM = '00C';  static readonly DATABASE_HOTEL = '009';  static readonly EVENT = '00F';  static readonly GANGWAY = '008';  static readonly GUEST = '006';  static readonly PROPERTY_SETUP = '00B';  static readonly ROOM = '00D';  static readonly USER_ANOVA = '007';}
 
