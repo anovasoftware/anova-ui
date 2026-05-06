633677b2d5cec717856786b607c296e0403dd1ff
anovasoftware <jburke@anovasoftware.com>
2026-05-06 13:32:38 -0400

    menu.1

---
+++ src/app/models/event.ts
@@ -0,0 +1,5 @@
+export interface Event {
+  eventId: string;
+  code: string;
+  description: string;
+}
--- src/app/models/hotel.ts
+++ src/app/models/hotel.ts
@@ -1,5 +1,8 @@
+import {Event} from './event';
+
 export interface Hotel {
   hotelId: string;
   code: string;
   description: string;
+  currentEvent: Event;
 }
