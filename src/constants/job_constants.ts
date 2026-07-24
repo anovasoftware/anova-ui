5fba68c0ef56c4a8a49c746a7d1fde4496dbf498
anovasoftware <jburke@anovasoftware.com>
2026-07-24 16:58:59 -0400

    currencies.1

--- src/constants/job_constants.ts
+++ src/constants/job_constants.ts
@@ -1,4 +1,4 @@
 export class JobConstants {
 // AUTO GENERATED
-  static readonly NOT_APPLICABLE = 'A00';  static readonly EVENT_ROOM_SERVICE = 'A03';  static readonly EXCHANGE_RATE_SERVICE = 'A02';  static readonly PERIOD_SERVICE = 'A01';}
+  static readonly NOT_APPLICABLE = 'A00';  static readonly CLIENT_SERVICE = 'A04';  static readonly EVENT_ROOM_SERVICE = 'A03';  static readonly EXCHANGE_RATE_SERVICE = 'A02';  static readonly PERIOD_SERVICE = 'A01';}
 
